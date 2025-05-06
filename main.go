fdc05633b96c587f81b764bd5fa58cd7398b82de
apetl <git@apetl.dev>
2025-05-05 21:09:48 -0500

    dir processing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 
@@ -45,13 +46,13 @@ func calcHash(path string) (string, error) {
 	return fmt.Sprintf("%x", hashSum), nil
 }
 
-func compare(filename string, filePath string, yamlPath string) (bool, bool, string, string, error) {
+func compare(filename string, filePath string, tracePath string) (bool, bool, string, string, error) {
 	currentHash, err := calcHash(filePath)
 	if err != nil {
 		return false, false, "", "", fmt.Errorf("failed to calculate hash: %w", err)
 	}
 
-	data, err := readTrace(yamlPath)
+	data, err := readTrace(tracePath)
 	if err != nil {
 		return false, false, currentHash, "", err
 	}
@@ -67,14 +68,14 @@ func compare(filename string, filePath string, yamlPath string) (bool, bool, str
 	return true, hashesMatch, currentHash, storedHash, nil
 }
 
-func readTrace(yamlPath string) (map[string]fileData, error) {
+func readTrace(tracePath string) (map[string]fileData, error) {
 	data := make(map[string]fileData)
 
-	if _, err := os.Stat(yamlPath); os.IsNotExist(err) {
+	if _, err := os.Stat(tracePath); os.IsNotExist(err) {
 		return data, nil
 	}
 
-	yamlBytes, err := os.ReadFile(yamlPath)
+	yamlBytes, err := os.ReadFile(tracePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read trace file: %w", err)
 	}
@@ -91,13 +92,13 @@ func readTrace(yamlPath string) (map[string]fileData, error) {
 	return data, nil
 }
 
-func writeTrace(data map[string]fileData, yamlPath string) error {
+func writeTrace(data map[string]fileData, tracePath string) error {
 	yamlBytes, err := yaml.Marshal(data)
 	if err != nil {
 		return fmt.Errorf("failed to marshal YAML: %w", err)
 	}
 
-	err = os.WriteFile(yamlPath, yamlBytes, 0644)
+	err = os.WriteFile(tracePath, yamlBytes, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to write file: %w", err)
 	}
@@ -105,10 +106,10 @@ func writeTrace(data map[string]fileData, yamlPath string) error {
 	return nil
 }
 
-func add(filePath string, yamlPath string) error {
+func addF(filePath string, tracePath string) error {
 	filename := filepath.Base(filePath)
 
-	isInTrace, hashesMatch, currentHash, storedHash, err := compare(filename, filePath, yamlPath)
+	isInTrace, hashesMatch, currentHash, storedHash, err := compare(filename, filePath, tracePath)
 	if err != nil {
 		return err
 	}
@@ -127,7 +128,7 @@ func add(filePath string, yamlPath string) error {
 		}
 	}
 
-	data, err := readTrace(yamlPath)
+	data, err := readTrace(tracePath)
 	if err != nil {
 		return err
 	}
@@ -136,11 +137,59 @@ func add(filePath string, yamlPath string) error {
 		Blake2b: currentHash,
 	}
 
-	return writeTrace(data, yamlPath)
+	hash, _ := calcHash(filePath)
+	fmt.Printf("\033[32mFile Added to Trace:\033[0m\n")
+	fmt.Printf("\033[36mFilename:\033[0m %s\n", filename)
+	fmt.Printf("\033[36mBlake2b Hash:\033[0m %s\n", hash)
+
+	return writeTrace(data, tracePath)
 }
 
-func del(filename string, yamlPath string) error {
-	data, err := readTrace(yamlPath)
+func add(path string) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("failed to access path: %w", err)
+	}
+
+	if fileInfo.IsDir() {
+		fmt.Printf("\033[36mProcessing directory:\033[0m %s\n", path)
+
+		dirTracePath := filepath.Join(path, ".trace")
+
+		err := filepath.WalkDir(path, func(filePath string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			return addF(filePath, dirTracePath)
+		})
+
+		if err != nil {
+			return fmt.Errorf("error processing directory: %w", err)
+		}
+
+		fmt.Printf("\033[36mSaved to:\033[0m %s\n", dirTracePath)
+		return nil
+	}
+
+	dirPath := filepath.Dir(path)
+	fileTracePath := filepath.Join(dirPath, ".trace")
+
+	err = addF(path, fileTracePath)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\033[36mSaved to:\033[0m %s\n", fileTracePath)
+	return nil
+}
+
+func delF(filename string, tracePath string) error {
+	data, err := readTrace(tracePath)
 	if err != nil {
 		return err
 	}
@@ -151,13 +200,61 @@ func del(filename string, yamlPath string) error {
 
 	delete(data, filename)
 
-	return writeTrace(data, yamlPath)
+	fmt.Printf("\033[32mFile Removed from Trace:\033[0m\n")
+	fmt.Printf("\033[36mFilename:\033[0m %s\n", filename)
+
+	return writeTrace(data, tracePath)
 }
 
-func check(filePath string, yamlPath string) error {
+func del(path string) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		dirPath := filepath.Dir(path)
+		fileTracePath := filepath.Join(dirPath, ".trace")
+		filename := filepath.Base(path)
+		return delF(filename, fileTracePath)
+	}
+
+	if fileInfo.IsDir() {
+		fmt.Printf("\033[36mProcessing directory:\033[0m %s\n", path)
+
+		dirTracePath := filepath.Join(path, ".trace")
+
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return fmt.Errorf("failed to read directory: %w", err)
+		}
+
+		for _, file := range files {
+			if !file.IsDir() {
+				err := delF(file.Name(), dirTracePath)
+				if err != nil {
+					fmt.Printf("\033[33mWarning:\033[0m %v\n", err)
+				}
+			}
+		}
+
+		fmt.Printf("\033[36mRemoved from:\033[0m %s\n", dirTracePath)
+		return nil
+	}
+
+	dirPath := filepath.Dir(path)
+	fileTracePath := filepath.Join(dirPath, ".trace")
+	filename := filepath.Base(path)
+
+	err = delF(filename, fileTracePath)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\033[36mRemoved from:\033[0m %s\n", fileTracePath)
+	return nil
+}
+
+func checkF(filePath string, tracePath string) error {
 	filename := filepath.Base(filePath)
 
-	isInTrace, hashesMatch, currentHash, storedHash, err := compare(filename, filePath, yamlPath)
+	isInTrace, hashesMatch, currentHash, storedHash, err := compare(filename, filePath, tracePath)
 	if err != nil {
 		return err
 	}
@@ -181,49 +278,74 @@ func check(filePath string, yamlPath string) error {
 	return nil
 }
 
+func check(path string) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("failed to access path: %w", err)
+	}
+
+	if fileInfo.IsDir() {
+		fmt.Printf("\033[36mChecking directory:\033[0m %s\n", path)
+
+		dirTracePath := filepath.Join(path, ".trace")
+
+		err := filepath.WalkDir(path, func(filePath string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			fmt.Printf("\n--- Checking %s ---\n", filePath)
+			return checkF(filePath, dirTracePath)
+		})
+
+		if err != nil {
+			return fmt.Errorf("error checking directory: %w", err)
+		}
+
+		return nil
+	}
+
+	dirPath := filepath.Dir(path)
+	fileTracePath := filepath.Join(dirPath, ".trace")
+
+	return checkF(path, fileTracePath)
+}
+
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Error: Please provide a command (add/del) and a file path.")
-		fmt.Println("Usage: program add|del filepath")
+		fmt.Println("Error: Please provide a command (add/del/check) and a file or directory path.")
+		fmt.Println("Usage: program add|del|check filepath")
 		os.Exit(1)
 	}
 
 	command := os.Args[1]
-	filePath := os.Args[2]
-	yamlPath := ".trace"
+	path := os.Args[2]
+
+	var err error
 
 	switch command {
 	case "add":
-		err := add(filePath, yamlPath)
+		err = add(path)
 		if err != nil {
-			fmt.Println("\033[31mError adding file to trace:\033[0m", err)
+			fmt.Println("\033[31mError adding to trace:\033[0m", err)
 			os.Exit(1)
 		}
-
-		filename := filepath.Base(filePath)
-		hash, _ := calcHash(filePath)
-
-		fmt.Println("\033[32mFile Added to Trace:\033[0m")
-		fmt.Printf("\033[36mFilename:\033[0m %s\n", filename)
-		fmt.Printf("\033[36mBlake2b Hash:\033[0m %s\n", hash)
-		fmt.Printf("\033[36mSaved to:\033[0m %s\n", yamlPath)
 
 	case "del":
-		filename := filepath.Base(filePath)
-		err := del(filename, yamlPath)
+		err = del(path)
 		if err != nil {
-			fmt.Println("\033[31mError removing file from trace:\033[0m", err)
+			fmt.Println("\033[31mError removing from trace:\033[0m", err)
 			os.Exit(1)
 		}
 
-		fmt.Println("\033[32mFile Removed from Trace:\033[0m")
-		fmt.Printf("\033[36mFilename:\033[0m %s\n", filename)
-		fmt.Printf("\033[36mRemoved from:\033[0m %s\n", yamlPath)
-
 	case "check":
-		err := check(filePath, yamlPath)
+		err = check(path)
 		if err != nil {
-			fmt.Println("\033[31mError comparing file:\033[0m", err)
+			fmt.Println("\033[31mError comparing:\033[0m", err)
 			os.Exit(1)
 		}
 
@@ -232,3 +354,4 @@ func main() {
 		os.Exit(1)
 	}
 }
+
